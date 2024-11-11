e00bb520e7c6ea6aaad734bdc825cfc94cb132d5
SzymonOzog <szymon.ozog@gmail.com>
2024-11-11 20:32:33 +0100

    in register kernel

--- kernels.cu
+++ kernels.cu
@@ -15,6 +15,11 @@
 #define SOFTMAX_VARIANT 8
 #endif
 
+#ifndef WIDTH
+#define WIDTH 0
+#endif
+
+#define CEILING(x,y) (((x) + (y) - 1) / (y))
 
 template <typename scalar_t>
 __global__ void softmax_kernel(scalar_t* __restrict__ a, scalar_t* __restrict__ b, int w, int h)
@@ -673,6 +678,123 @@ __global__ void softmax_kernel9(scalar_t* __restrict__ a, scalar_t* __restrict__
   }
 }
 
+
+template <typename scalar_t>
+__global__ void softmax_kernel10(scalar_t* __restrict__ a, scalar_t* __restrict__ b, int w, int h)
+{
+  int row = blockIdx.x;
+  int ty = threadIdx.y;
+  int warp_id = ty/32;
+  int lane_id = ty%32;
+  
+  __shared__ float reduction[BLOCK_DIM_Y/32]; 
+  float4 reg_array[CEILING((WIDTH/4),BLOCK_DIM_Y)];
+
+  int reg_array_idx = 0;
+  if (row < h)
+  {
+    float maxval = 0;
+#pragma unroll 1
+    for (int i = ty; i<WIDTH/4; i+=BLOCK_DIM_Y)
+    {
+      float4 val = reinterpret_cast<float4*>(&a[row*WIDTH + i*4])[0];
+      maxval = fmaxf(maxval, val.x);
+      maxval = fmaxf(maxval, val.y);
+      maxval = fmaxf(maxval, val.z);
+      maxval = fmaxf(maxval, val.w);
+      reg_array[reg_array_idx] = val;
+      reg_array_idx+=1;
+    }
+    maxval = fmaxf(maxval, __shfl_xor_sync(0xffffffff, maxval, 16, 32));
+    maxval = fmaxf(maxval, __shfl_xor_sync(0xffffffff, maxval, 8, 32));
+    maxval = fmaxf(maxval, __shfl_xor_sync(0xffffffff, maxval, 4, 32));
+    maxval = fmaxf(maxval, __shfl_xor_sync(0xffffffff, maxval, 2, 32));
+    maxval = fmaxf(maxval, __shfl_xor_sync(0xffffffff, maxval, 1, 32));
+
+    if (lane_id == 0)
+    {
+      reduction[warp_id] = maxval;
+    }
+    __syncthreads();
+    if (warp_id == 0)
+    {
+        maxval = ty < BLOCK_DIM_Y/32 ? reduction[ty] : 0;
+        maxval = fmaxf(maxval, __shfl_xor_sync(0xffffffff, maxval, 16, 32));
+        maxval = fmaxf(maxval, __shfl_xor_sync(0xffffffff, maxval, 8, 32));
+        maxval = fmaxf(maxval, __shfl_xor_sync(0xffffffff, maxval, 4, 32));
+        maxval = fmaxf(maxval, __shfl_xor_sync(0xffffffff, maxval, 2, 32));
+        maxval = fmaxf(maxval, __shfl_xor_sync(0xffffffff, maxval, 1, 32));
+    }
+    if (ty == 0)
+    {
+        reduction[0] = maxval;
+    }
+    __syncthreads();
+    maxval = reduction[0];
+    float divisor = 0.f;
+    reg_array_idx=0;
+#pragma unroll 1
+    for (int i = ty; i<WIDTH/4; i+=BLOCK_DIM_Y)
+    {
+        float4 val = reg_array[reg_array_idx];
+        val.x = __expf(val.x - maxval);
+        val.y = __expf(val.y - maxval);
+        val.z = __expf(val.z - maxval);
+        val.w = __expf(val.w - maxval);
+        divisor += val.x;
+        divisor += val.y;
+        divisor += val.z;
+        divisor += val.w;
+        reg_array[reg_array_idx] = val;
+      reg_array_idx+=1;
+    }
+
+    divisor += __shfl_xor_sync(0xffffffff, divisor, 16, 32);
+    divisor += __shfl_xor_sync(0xffffffff, divisor, 8, 32);
+    divisor += __shfl_xor_sync(0xffffffff, divisor, 4, 32);
+    divisor += __shfl_xor_sync(0xffffffff, divisor, 2, 32);
+    divisor += __shfl_xor_sync(0xffffffff, divisor, 1, 32);
+
+    if (lane_id == 0)
+    {
+      reduction[warp_id] = divisor;
+    }
+
+    __syncthreads();
+    if (warp_id == 0)
+    {
+        divisor = ty < BLOCK_DIM_Y/32 ? reduction[ty] : 0;
+        divisor += __shfl_xor_sync(0xffffffff, divisor, 16, 32);
+        divisor += __shfl_xor_sync(0xffffffff, divisor, 8, 32);
+        divisor += __shfl_xor_sync(0xffffffff, divisor, 4);
+        divisor += __shfl_xor_sync(0xffffffff, divisor, 2);
+        divisor += __shfl_xor_sync(0xffffffff, divisor, 1);
+    }
+
+    if (ty == 0)
+    {
+        reduction[0] = divisor;
+    }
+
+    __syncthreads();
+    divisor = reduction[0];
+
+    reg_array_idx = 0;
+#pragma unroll 1
+    for (int i = ty; i<WIDTH/4; i+=BLOCK_DIM_Y)
+    {
+        float4 val = reg_array[reg_array_idx];
+        val.x = val.x/divisor;
+        val.y = val.y/divisor;
+        val.z = val.z/divisor;
+        val.w = val.w/divisor;
+        reinterpret_cast<float4*>(&b[row*WIDTH + i*4])[0] = val;
+      reg_array_idx+=1;
+    }
+
+  }
+}
+
 torch::Tensor softmax_cu(torch::Tensor x)
 {
   auto out = torch::empty_like(x);
@@ -736,6 +858,13 @@ torch::Tensor softmax_cu(torch::Tensor x)
 #if SOFTMAX_VARIANT == 9
   AT_DISPATCH_FLOATING_TYPES(x.type(), "softmax_cuda", ([&] {
         softmax_kernel9<scalar_t><<<grid_size, block_size>>>
+          (x.data_ptr<scalar_t>(), out.data_ptr<scalar_t>(), w, h);
+        }));
+#endif
+
+#if SOFTMAX_VARIANT == 10
+  AT_DISPATCH_FLOATING_TYPES(x.type(), "softmax_cuda", ([&] {
+        softmax_kernel10<scalar_t><<<grid_size, block_size>>>
           (x.data_ptr<scalar_t>(), out.data_ptr<scalar_t>(), w, h);
         }));
 #endif
